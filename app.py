b65bcc2cd89c28de7201a187c4b7b5b1d36b8450
omkar3311 <omkarwaghmare3311@gmail.com>
2026-01-16 20:15:02 +0530

    choices

--- app.py
+++ app.py
@@ -6,30 +6,45 @@ file = st.file_uploader("upload file" ,type=["csv"])
 seats = st.number_input("seat limit",value=10)
 button = st.button("Upload")
 
-major = ["aids" , "cs" , "civil" , "mech" ]
+major = ["aids" , "cs" , "civil" , "mech" ,"e&tc"]
 minor = {
-    "aids" : ["cs" , "civil" , "mech" ],
-    "cs" : ["civil" , "mech" ],
-    "civil" : ["mech"],
-    "mech" : ["civil"]
+    "aids" : ["e&tc" , "civil" , "mech" ],
+    "cs" : ["e&tc","civil" , "mech" ],
+    "civil" : ["aids","cs","e&tc","mech"],
+    "mech" : ["aids","cs","e&tc","civil"]
 }
 
 if file and button:
     data = pd.read_csv(file)
-    st.write("Uploaded Data")
+    st.write("Uploaded data")
     st.dataframe(data)
-    sorted_data = data.sort_values(by = ["Backlog" , "Percentage"] , ascending = [True,False])
-    grouped_data = sorted_data.groupby("Branch", sort=False)
-    grouped_data = {branch: group.copy() for branch, group in sorted_data.groupby("Branch", sort=False)}
-    for branch, df in grouped_data.items():
-        allowed_minors = minor[branch]
-        df["Minor"] = None 
-        for i in range(len(df)):
-            minor_idx = i // seats
-            if minor_idx < len(allowed_minors):
-                df.iloc[i, df.columns.get_loc("Minor")] = allowed_minors[minor_idx]
-            else:
-                df.iloc[i, df.columns.get_loc("Minor")] = None
-    final_data = (pd.concat(grouped_data.values(), ignore_index=True).sort_index())
+    data = data.sort_values(by=["Backlog", "Percentage"],ascending=[True, False])
+    data["Minor"] = None
+    minor_seats = { branch: {m: seats for m in minors} for branch, minors in minor.items()}
+    
+    for branch in data["Branch"].unique():
+        branch_students = data[data["Branch"] == branch]
+        
+        for idx, row in branch_students.iterrows():
+            assigned = False
+
+            for choice_col in ["Choice1", "Choice2", "Choice3"]:
+                choice = row.get(choice_col)
+                if ( choice in minor_seats[branch] and minor_seats[branch][choice] > 0):
+                    data.at[idx, "Minor"] = choice
+                    minor_seats[branch][choice] -= 1
+                    assigned = True
+                    break
+
+            if not assigned:
+                for m, count in minor_seats[branch].items():
+                    if count > 0:
+                        data.at[idx, "Minor"] = m
+                        minor_seats[branch][m] -= 1
+                        assigned = True
+                        break
+
+    final_data = data.sort_values(by=["Branch", "Backlog", "Percentage"],ascending=[True, True, False]).reset_index(drop=True)
+
     st.write("Assigned minor")
     st.dataframe(final_data)
