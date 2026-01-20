874fa440dea93062a332c70d069236299d4f9169
omkar3311 <omkarwaghmare3311@gmail.com>
2026-01-20 19:48:59 +0530

    preprocess

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from fastapi import FastAPI, UploadFile, File, Form, Request, HTTPException
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 import pandas as pd
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
